f1ff5f6147a8919b6989e8de43c368ac03b9c0f0
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-12-08 13:03:20 +0530

    setup for the new challenge

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-lg font-bold text-center text-blue-600'>Hello, there get ready for some really exicting stuff!</p>
+    </div>
+  )
+}
+
+export default App
