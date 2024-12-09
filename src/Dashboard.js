3cae60185606ad270d2b52ac2158d9a2dd018785
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-12-09 20:02:49 +0530

    creating the information for each user

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EmpCard from './EmpCard'
 import { empData } from './constant'
 
 const Dashboard = () => {
+  const [onclickData, setonclickData] = useState(null);
+  console.log("Data", onclickData);
   return (
       <div className='flex justify-center items-center'>
           <div className='flex border-[1px] border-white w-[86rem] h-[38rem] mt-4'>
               <div className='w-[30%] h-full overflow-y-scroll scroll-smooth'>
                   {empData.map((i) => {
-                      return <EmpCard key={i?.id} data={i} />
+                    return <EmpCard key={i?.id} data={i} onClick={()=>setonclickData(i)} />
                   })
                   }
               </div>
-              <div className='w-[70%] h-full'></div>
+        <div className='w-[70%] h-full'>
+          {onclickData&&<div className='flex flex-col justify-center items-center'>
+            <img src='https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png' className='w-[15rem]' />
+            <div className='flex'>
+              <p className='text-white font-bold text-4xl mr-2'>{onclickData?.firstName}</p>
+              <p className='text-white font-bold text-4xl'>{onclickData?.lastName}</p>
+            </div>
+              <p className='text-white font-bold text-2xl mt-1 ml-2'>{onclickData?.Designation}</p>
+            <p className='text-white font-bold text-xl mt-1 ml-2'>{onclickData?.dob}</p>
+            <p className='text-white font-bold text-xl mt-1 ml-2'>{onclickData?.sex}</p>
+              <p className='text-white font-bold text-xl mt-1 ml-2'>{onclickData?.phoneNumber}</p>
+             
+          </div>}
+              </div>
       </div>
     </div>
   )
--- src/constant.js
+++ src/constant.js
@@ -5,6 +5,8 @@ export const empData = [
         lastName: "Sharma",
         dob: "04/10/2000",
         phoneNumber: "7042981599",
+        sex: "Male",
+        Designation:"Engineer"
     },
     {
         id: 2,
@@ -12,13 +14,17 @@ export const empData = [
         lastName: "Sharma",
         dob: "16/04/2002",
         phoneNumber: "8929818752",
+        sex: "Male",
+        Designation:"Pharmacist"
     },
     {
         id: 3,
         firstName: "Pranjal",
-        lastName: "Prakash",
+        lastName: "Dhaiya",
         dob: "06/02/1995",
         phoneNumber: "9629815851",
+        sex: "Female",
+        Designation:"Consultant"
     },
     {
         id: 4,
@@ -26,6 +32,8 @@ export const empData = [
         lastName: "Chandan",
         dob: "09/08/1998",
         phoneNumber: "9541287218",
+        sex: "Male",
+        Designation:"Manager"
     },
     {
         id: 5,
@@ -33,6 +41,8 @@ export const empData = [
         lastName: "Mishra",
         dob: "26/09/2000",
         phoneNumber: "9949888727",
+        sex: "Female",
+        Designation:"Nurse"
     },
     {
         id: 6,
@@ -40,6 +50,8 @@ export const empData = [
         lastName: "Bansal",
         dob: "19/02/2001",
         phoneNumber: "9529712720",
+        sex: "Male",
+        Designation:"Doctor"
     },
     {
         id: 7,
@@ -47,5 +59,7 @@ export const empData = [
         lastName: "Kaur",
         dob: "28/11/2000",
         phoneNumber: "9829818752",
+        sex: "Female",
+        Designation:"HR"
     },
 ];
