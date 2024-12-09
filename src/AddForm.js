4a1d2f43bf44671a7aaca6d814fdaef1e53106fd
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-12-09 23:04:23 +0530

    creating the form for adding new employee

---
+++ src/AddForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddForm = () => {
+  return (
+    <div className='flex justify-center items-center'>
+          <div className='absolute bg-black w-[25rem] h-[15rem]'>     
+      </div>
+    </div>
+  )
+}
+
+export default AddForm
--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Dashboard from './Dashboard'
+import AddForm from './AddForm';
 
 const App = () => {
+  const [addNew, setAddNew] = useState(false);
   return (
     <div>
-      <p className='font-bold text-2xl text-white ml-20 mt-5'>EMPLOYEE MANAGEMENT SYSTEM</p>
+      <div className='flex mt-5 justify-between'>
+        <p className='font-bold text-3xl text-white ml-20'>EMPLOYEE MANAGEMENT SYSTEM</p>
+        <button className='mr-28 bg-red-500 text-white text-xl font-bold px-5 py-2 rounded-full hover:scale-y-110'onClick={()=>setAddNew(true)}>ADD</button>
+      </div>
+      {addNew&&<AddForm/>}
       <div>
         <Dashboard/>
       </div>
